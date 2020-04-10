c1e83d7411ad61721597c47c408a942868c6d7a2
Kirbyasdf <kirbyAugustus@gmail.com>
2020-04-10 06:02:15 -0400

    socket testing

--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+
 import "../styles/chat/chat.css";
 import MessageForm from "../components/chat/MessageForm";
 import Message from "../components/chat/Message";
 
 export default function ChatContainer() {
   const [log, setLog] = useState([]);
+  const [chatroom, setChatroom] = useState("tru baller after hours");
+  const [user, setUser] = useState("ballest fool in da street");
+
+  useEffect(() => {
+    const socket = io("localhost:5000");
+    socket.emit("yo");
+  }, [log]);
 
   const handleMessageSubmit = (message) => {
     setLog([...log, message]);
