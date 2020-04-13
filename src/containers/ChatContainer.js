88c28331aad14278d9564b06d24745d05c33b952
Kirbyasdf <kirbyAugustus@gmail.com>
2020-04-12 20:06:55 -0400

    fixed double sending messages

--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -17,26 +17,28 @@ export default function ChatContainer() {
   const socket = io(URL);
   /// default function loads in chatroom and the log we can use this logic to render other chat rooms in the future
 
-  /// I need to get it so that the messages load correctly...right now they are loading as for other users even when they are not
   useEffect(() => {
-    async function fetchDefaulChatroom() {
-      try {
-        const chatroomRes = await (
-          await fetch(URL + "/chatrooms/defaultchatroom")
-        ).json();
-
-        await setChatroom(chatroomRes);
-        const logRes = await (
-          await fetch(URL + "/messages/fetchlog/" + chatroomRes.id)
-        ).json();
-        console.log(logRes);
-        await setLog(logRes);
-      } catch (err) {
-        console.error(err);
-      }
-    }
     fetchDefaulChatroom();
   }, []);
+
+  ///moved this outside so that the function isnt defined everysingle time
+  const fetchDefaulChatroom = async () => {
+    try {
+      // this  will be in one fetch call eventually as in on the backend we will pull a single chatroom and all of its messages in a join call and send that as the json, for now we are just playing with the default test room
+      const chatroomRes = await (
+        await fetch(URL + "/chatrooms/defaultchatroom")
+      ).json();
+      await setChatroom(chatroomRes);
+      /// I DONT KNOW IF SHOULD BE or even can await set state lol
+
+      const logRes = await (
+        await fetch(URL + "/messages/fetchlog/" + chatroomRes.id)
+      ).json();
+      await setLog(logRes);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   ///TO DO get messages from backend after they are persisted then broadcasted + auto fill chat history
   useEffect(() => {
@@ -50,7 +52,6 @@ export default function ChatContainer() {
     message["chatroom_id"] = chatroom.id;
     message["user_id"] = user.id;
     await socket.emit("sendMessage", message, () => {});
-    setLog([...log, message]);
   };
 
   /// need to find a way to pass username with the user_id... because the sql return isn't like active record..I cant pull all the user info with it... I have to make a join query...will do more reseach on this.. in the meantime I am sorting by user id
